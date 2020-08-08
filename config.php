a1bde2298ba57df76592439c2a879c0bf5809026
Johnny Stenson <johnjstenson@gmail.com>
2020-08-08 17:19:10 -0400

    mutiple login

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-/** Set default timezone <== important! */
+
 date_default_timezone_set('America/New_York');
 
 /** Include external dependencies */
@@ -12,14 +12,11 @@ Debugger::enable();
 
 /** Load configuration settings from __DIR__/.env */
 try {
-
     $dotenv = Dotenv\Dotenv::create(__DIR__);
     $dotenv->load();
 
 } catch (Exception $e) {
-
     print $e->getMessage();
-
 }
 
 Debugger::enable(Debugger::DEVELOPMENT,__DIR__);
@@ -27,14 +24,25 @@ Debugger::$showLocation = true;
 Debugger::$maxLength = 256;
 Debugger::$maxDepth = 4;
 
-defined("SITE_PW") or define("SITE_PW", getenv('SITE_PW'));
-defined("USERNAME") or define("USERNAME", getenv('USERNAME'));
-defined("SECURITY_TOKEN") or define("SECURITY_TOKEN", getenv('SECURITY_TOKEN'));
-defined("PASSWORD") or define("PASSWORD", getenv('PASSWORD'));
+/**
+ * Salesforce Credentials 
+ */
+
+// CREW
+defined("CREW_SITEPW") or define("CREW_SITEPW", getenv('CREW_SITEPW'));
+defined("CREW_USERNAME") or define("CREW_USERNAME", getenv('CREW_USERNAME'));
+defined("CREW_PASSWORD") or define("CREW_PASSWORD", getenv('CREW_PASSWORD'));
+defined("CREW_SECURITY_TOKEN") or define("CREW_SECURITY_TOKEN", getenv('CREW_SECURITY_TOKEN'));
+
+// MANAGER
+defined("MNGR_SITEPW") or define("MNGR_SITEPW", getenv('MNGR_SITEPW'));
+defined("MNGR_USERNAME") or define("MNGR_USERNAME", getenv('MNGR_USERNAME'));
+defined("MNGR_PASSWORD") or define("MNGR_PASSWORD", getenv('MNGR_PASSWORD'));
+defined("MNGR_SECURITY_TOKEN") or define("MNGR_SECURITY_TOKEN", getenv('MNGR_SECURITY_TOKEN'));
+
 require_once ('soapclient/SforcePartnerClient.php');
 
 $mySforceConnection = new SforcePartnerClient();
 $mySforceConnection->createConnection("/home/thunde91/lightning.thunderroadinc.com/inventory/soapclient/partner.wsdl.xml");
-$mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 
 use Intervention\Image\ImageManager;
--- site-auth.php
+++ site-auth.php
@@ -1,7 +1,24 @@
 <?php
 require_once 'config.php';
-if((isset($_POST['pw']) && SITE_PW == $_POST['pw']) || $_SESSION['site_auth']){
-    $_SESSION['site_auth'] = TRUE;
+
+if(isset($_POST['pw'])){
+    switch(filter_var($_POST['pw'], FILTER_SANITIZE_STRING)){
+        case CREW_SITEPW:
+            $_SESSION['site_auth'] = TRUE;
+            $_SESSION['role'] = 'CREW';
+        break;
+        case MNGR_SITEPW:
+            $_SESSION['site_auth'] = TRUE;
+            $_SESSION['role'] = 'MNGR';
+        break;
+        default:
+        die('<body><h1 style="margin:50px; text-aling:center;">Nope <br /> Não</h1></body>');
+    }
+    $mySforceConnection->login(
+        constant($_SESSION['role'] . _USERNAME), 
+        constant($_SESSION['role'] . _PASSWORD)
+        .constant($_SESSION['role'] . _SECURITY_TOKEN)
+    );
 }else{
-    die('Nope / Não');
+    if(!$_SESSION['site_auth']) die('<body><h1 style="margin:50px; text-aling:center;">Nope <br /> Não</h1></body>');
 }
